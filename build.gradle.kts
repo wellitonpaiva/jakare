18eba01c76100291ee8800052d9cdfde924faff9
Welliton Paiva <wellitonpaiva@gmail.com>
2024-08-10 09:23:09 +0100

    removing native and running ktor html and htmx

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,18 @@
-
 val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.0"
     id("io.ktor.plugin") version "2.3.11"
+    application
 }
 
 group = "jakare"
 version = "0.0.1"
 
 application {
-    mainClass.set("jakare.ApplicationKt")
-
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+    mainClass = "jakare.ApplicationKt"
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=true")
 }
 
 repositories {
@@ -24,6 +22,8 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-html-builder")
+    implementation("org.webjars.npm:htmx.org:1.9.4")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
--- src/main/kotlin/jakare/Application.kt
+++ src/main/kotlin/jakare/Application.kt
@@ -3,7 +3,9 @@ package jakare
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import jakare.plugins.*
+import io.ktor.server.routing.*
+import io.ktor.server.html.*
+import kotlinx.html.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -11,5 +13,36 @@ fun main() {
 }
 
 fun Application.module() {
-    configureRouting()
+    routing {
+        get("/") {
+            call.respondHtml {
+                head {
+                    script(type = ScriptType.textJScript, src= "https://unpkg.com/htmx.org@2.0.1" ) {}
+                    link(rel = "stylesheet", href = "https://cdn.simplecss.org/simple.min.css")
+                }
+                body("jakare")
+
+            }
+        }
+        post("/pressed") {
+            call.respondHtml { body("jakared") }
+        }
+    }
+}
+
+private fun HTML.body(h1: String) {
+    body {
+        id="theBody"
+        div {
+            h1 {
+                id = "title"
+                +h1
+            }
+            button {
+                attributes["hx-post"] = "/pressed"
+                attributes["hx-target"] = "#theBody"
+                +"Don't press this button"
+            }
+        }
+    }
 }
