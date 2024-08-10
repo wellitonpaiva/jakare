2af7832491263ae9bb1027705d46e90fbecd4b9e
Welliton Paiva <wellitonpaiva@gmail.com>
2024-08-10 15:16:43 +0100

    adding reptiles list

---
+++ README.md
@@ -0,0 +1,3 @@
+# KTOR + kotlinx HTML + HTMX playground
+
+csv file copied from: https://www.ecoregistros.org/site_en/ordenalfabetico.php?id=4&page=3
--- src/main/kotlin/jakare/Application.kt
+++ src/main/kotlin/jakare/Application.kt
@@ -5,44 +5,101 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.routing.*
 import io.ktor.server.html.*
+import io.ktor.server.response.*
 import kotlinx.html.*
+import kotlinx.html.stream.createHTML
+
+data class Reptile(
+    val order: String,
+    val family: String,
+    val englishCommonName: String,
+    val spanishCommonName: String,
+    val portugueseCommonName: String,
+    val scientificName: String,
+)
+
+fun String.toReptile(): Reptile {
+    val rep = this.split(",")
+    return Reptile(
+        order = rep[0],
+        family = rep[1],
+        englishCommonName = rep[2],
+        spanishCommonName = rep[3],
+        portugueseCommonName = rep[4],
+        scientificName = rep[5]
+    )
+}
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    val reptiles: List<Reptile> = {}.javaClass.getResource("/reptiles.csv")
+        ?.openStream()
+        ?.bufferedReader()
+        ?.readLines()
+        ?.map(String::toReptile)
+        .orEmpty()
+
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = { module(reptiles) })
         .start(wait = true)
 }
 
-fun Application.module() {
+fun Application.module(reptiles: List<Reptile>) {
     routing {
         get("/") {
-            call.respondHtml {
-                head {
-                    script(type = ScriptType.textJScript, src= "https://unpkg.com/htmx.org@2.0.1" ) {}
-                    link(rel = "stylesheet", href = "https://cdn.simplecss.org/simple.min.css")
+            call.respondHtmlTemplate(LayoutTemplate()) {
+                content {
+                    input(type = InputType.search) {
+                        name = "search"
+                        placeholder = "Search by name"
+                        attributes["hx-get"] = "/list"
+                        attributes["hx-trigger"] = "input changed delay:500ms, search"
+                        attributes["hx-target"] = "#list"
+                    }
+                    div { id="list" }
                 }
-                body("jakare")
-
             }
         }
-        post("/pressed") {
-            call.respondHtml { body("jakared") }
+
+        get("/list") {
+            call.respondText { reptiles.search(call.request.queryParameters["search"].orEmpty()) }
         }
     }
 }
 
-private fun HTML.body(h1: String) {
-    body {
-        id="theBody"
-        div {
-            h1 {
-                id = "title"
-                +h1
+fun List<Reptile>.search(search: String) =
+        createHTML().table {
+            id = "search"
+            tr {
+                th { +"Order" }
+                th { +"Family" }
+                th { +"English Common Name" }
+                th { +"Spanish Common Name" }
+                th { +"Portuguese Common Name" }
+                th { +"Scientific Name" }
             }
-            button {
-                attributes["hx-post"] = "/pressed"
-                attributes["hx-target"] = "#theBody"
-                +"Don't press this button"
-            }
+            filter { if (search.isNotBlank()) it.scientificName.contains(search) else true}
+                .map {
+                    tr {
+                        td { +it.order }
+                        td { +it.family }
+                        td { +it.englishCommonName }
+                        td { +it.spanishCommonName }
+                        td { +it.portugueseCommonName }
+                        td { +it.scientificName }
+                    }
+                }
+        }
+
+
+class LayoutTemplate : Template<HTML> {
+    val content = Placeholder<FlowContent>()
+    override fun HTML.apply() {
+        head {
+            script(type = ScriptType.textJScript, src = "https://unpkg.com/htmx.org@2.0.1") {}
+            link(rel = "stylesheet", href = "https://cdn.simplecss.org/simple.min.css")
+        }
+        body {
+            h1 { +"JAKARE" }
+            div { insert(content) }
         }
     }
 }
